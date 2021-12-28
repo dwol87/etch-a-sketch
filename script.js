73f2be283e4831e99b5a820fe3b89b3786ab20a3
David Wolinsky <dwolinsky87@gmail.com>
2021-12-28 16:06:14 -0500

    12/28/21

---
+++ script.js
@@ -0,0 +1,18 @@
+function buildGrid(x, y, cellSize, gridElement) {
+    gridElement.style.display = "grid";
+    gridElement.style.gridTemplateColumns = `repeat(${x}, ${cellSize}px)`;
+    gridElement.style.gridTemplateRows = `repeat(${y}, ${cellSize}px)`;
+   
+    let squares = new DocumentFragment();
+  
+    for (let i = 0; i < x * y; i++) {
+      let square = document.createElement('div');
+      square.className = 'square';
+      squares.appendChild(square);
+    }
+  
+    gridElement.appendChild(squares);
+  }
+  
+  
+  buildGrid(16, 16, 25,  document.querySelector(".grid"));
